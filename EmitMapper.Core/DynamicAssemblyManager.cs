96348acfd08cb9cf510e76bf159c596199af283f
guodf <guo_net_win@live.com>
2018-08-09 11:25:51 +0800

    Migrate to .Net Core

---
+++ EmitMapper.Core/DynamicAssemblyManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Reflection;
+using EmitMapper.Mappers;
+
+namespace EmitMapper
+{
+	/// <summary>
+	/// Class which maintains an assembly for created object Mappers
+	/// </summary>
+	public class DynamicAssemblyManager
+	{
+		/// <summary>
+		/// Saves assembly with created Mappers to file. This method is useful for debugging purpose.
+		/// </summary>
+		public static void SaveAssembly()
+		{
+            throw new NotImplementedException("DynamicAssemblyManager.SaveAssembly");
+		}
+
+		#region Non-public members
+
+		private static AssemblyName assemblyName;
+		private static AssemblyBuilder assemblyBuilder;
+		private static ModuleBuilder moduleBuilder;
+
+		static DynamicAssemblyManager()
+		{
+            assemblyName = new AssemblyName("EmitMapperAssembly");
+
+            assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndCollect);
+			moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name);
+        }
+
+		private static string CorrectTypeName(string typeName)
+		{
+			if (typeName.Length >= 1042)
+			{
+				typeName = "type_" + typeName.Substring(0, 900) + Guid.NewGuid().ToString().Replace("-", "");
+			}
+			return typeName;
+		}
+
+		public static TypeBuilder DefineMapperType(string typeName)
+		{
+			lock (typeof(DynamicAssemblyManager))
+			{
+				return moduleBuilder.DefineType(
+					CorrectTypeName(typeName + Guid.NewGuid().ToString().Replace("-", "")),
+					TypeAttributes.Public,
+					typeof(MapperForClassImpl),
+					null
+					);
+			}
+		}
+
+		public static TypeBuilder DefineType(string typeName, Type parent)
+		{
+			lock (typeof(DynamicAssemblyManager))
+			{
+				return moduleBuilder.DefineType(
+					CorrectTypeName(typeName),
+					TypeAttributes.Public,
+					parent,
+					null
+					);
+			}
+		}
+		#endregion
+	}
+}
---
+++ SamplesTests.Core/TestObjectsTracker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EmitMapperTests;
+using Xunit;
+
+namespace LightDataAccess
+{
+	
+	public class TestObjectsTracker
+	{
+		public class A
+		{
+			public string f1 = "f1";
+			public int f2 = 2;
+			public bool f3 = true;
+		}
+
+		[Fact]
+		public void TestObjectsTracking()
+		{
+			var tracker = new ObjectsChangeTracker();
+			var a = new A();
+			tracker.RegisterObject(a);
+			a.f2 = 3;
+			var changes = tracker.GetChanges(a);
+			Assert.True( changes[0].name == "f2");
+			tracker.RegisterObject(a);
+			changes = tracker.GetChanges(a);
+			Assert.True(changes.Length == 0);
+
+			a.f1 = "new";
+			a.f2 = 13;
+			a.f3 = false;
+			for (int i = 0; i < 10; ++i)
+			{
+				tracker.GetChanges(a);
+			}
+
+			changes = tracker.GetChanges(a);
+            
+			Assert.True(TestUtils.AreEqualArraysUnordered(new[] { "f1", "f2", "f3" }, changes.Select(c => c.name).ToArray()));
+
+			changes = tracker.GetChanges(new A());
+			Assert.Null(changes);
+		}
+	}
+}
